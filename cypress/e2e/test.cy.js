8278f838278fb811284734b0e6e7b7310517fc5d
Viraj6937 <utekar29viraj@gmail.com>
2024-01-11 11:34:00 +0530

    updated test spec file

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -20,32 +20,33 @@ describe('kennect.io Assignment',()=>{
         //click event on login submit to login as user
         lpo.getLoginSubmit().click();
 
-        // //Add Test
-        // cy.wait(3000);
-        // dashboard_board.getSideNav("Tests");
-        // dashboard_board.addTest("abc","SERUM","1ml","None",100);
-        // cy.wait(2000)
-        // // //Add Labs
-        // dashboard_board.getSideNav("Labs");
-        // dashboard_board.addLab("test5","test5@gmail.com",9292929292,"https://www.google.com","test12","cityTest",40000,"Maharashtra","abc");
-        // cy.wait(2000)
-        // // //Add Doctors
-        // dashboard_board.getSideNav("Doctors");
-        // dashboard_board.addDoctor("Deepak","deepak123@gmail.com","9494949494","MBBS","Doctor","Shree Clinic","14,Test123",400201,"Mumbai");
-        // cy.wait(2000)
-        // // //Add Inventory
-        //  dashboard_board.getSideNav("Inventory");
-        //  dashboard_board.addInventory("loreum Ipsum 2024","test123456",1000);
-        //  cy.wait(2000)
-        // // //Add Patient
-        //  dashboard_board.getSideNav("Patients");
-        //  dashboard_board.addPatient("sushil","sushil123@gmail.com",9292929292,152.4, 68,"Male",29,70,50,"abc","10%","test5","Deepak","10%","loreum Ipsum 2024");
-        //  cy.wait(2000)
-        // // Validate Cost of Test
-        //    dashboard_board.getSideNav("Dashboard");
-        //    cy.wait(4000);
-        //    dashboard_board.costCalculator("abc","10%");
-        // check correct user is signed in successfully
+        //Add Test
+        cy.wait(3000);
+        dashboard_board.getSideNav("Tests");
+        dashboard_board.addTest("abc","SERUM","1ml","None",100);
+        cy.wait(2000)
+        //Add Labs
+        dashboard_board.getSideNav("Labs");
+        dashboard_board.addLab("test5","test5@gmail.com",9292929292,"https://www.google.com","test12","cityTest",40000,"Maharashtra","abc");
+        cy.wait(2000)
+        //Add Doctors
+        dashboard_board.getSideNav("Doctors");
+        dashboard_board.addDoctor("Deepak","deepak123@gmail.com","9494949494","MBBS","Doctor","Shree Clinic","14,Test123",400201,"Mumbai");
+        cy.wait(2000)
+        //Add Inventory
+         dashboard_board.getSideNav("Inventory");
+         dashboard_board.addInventory("loreum Ipsum 2024","test123456",1000);
+         cy.wait(2000)
+        //Add Patient
+         dashboard_board.getSideNav("Patients");
+         dashboard_board.addPatient("sushil","sushil123@gmail.com",9292929292,152.4, 68,"Male",29,70,50,"abc","10%","test5","Deepak","10%","loreum Ipsum 2024");
+         cy.wait(2000)
+        // Validate Cost of Test
+           dashboard_board.getSideNav("Dashboard");
+           cy.wait(4000);
+           dashboard_board.costCalculator("abc","10%");
+  
+           // check correct user is signed in successfully
         cy.wait(4000);
         cy.get('div.MuiAvatar-circle').click();
         cy.get("div.MuiPopover-paper > div > div:nth-Child(3)").then(($element)=>{
